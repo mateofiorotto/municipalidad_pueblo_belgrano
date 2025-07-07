392743a61f11f47945d8376208b62b6da9a53d4f
Mateo Fiorotto <mateofiorotto2004@gmail.com>
2025-07-07 12:15:08 -0300

    Iniciando proyecto

---
+++ Backend/municipalidad/src/test/java/ar/gob/pueblogeneralbelgrano/municipalidad/MunicipalidadApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.gob.pueblogeneralbelgrano.municipalidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunicipalidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
